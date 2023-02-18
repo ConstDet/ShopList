851cf8ebad56f7f1bbf41db5b478d4ac9beccfac
ConstDet <constx7@gmail.com>
2023-02-18 18:43:51 +0300

    Программа ShopList завершена

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,96 @@
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Scanner;
+import java.util.List;
 
 public class Main {
+    protected static List<String> listOperation, listProduct;
+    protected static List<String> listBasket = new ArrayList<>();//создаем корзину;
+    protected static Scanner scanner = new Scanner(System.in);
+
+    public static void addProductBasket() {
+        Iterator<String> iteratorProduct = listProduct.iterator();
+        System.out.println("Укажите номер товара или введите stop:");
+        while (iteratorProduct.hasNext()) {
+            String s = iteratorProduct.next();
+            System.out.println(listProduct.indexOf(s) + 1 +". " + s);
+        }
+        System.out.print("Какую покупку хотите добавить? ");
+        System.out.println("...или введите stop");
+    }
+
+    public static void showBasket() {
+        Iterator<String> iteratorBasked = listBasket.iterator();
+        System.out.println("Список покупок: ");
+        while (iteratorBasked.hasNext()) {
+            String s = iteratorBasked.next();
+            System.out.println(listBasket.indexOf(s) + 1 +". " + s);
+        }
+    }
+
+    public static void deleteProduct() {
+        System.out.println("Какую хотите удалить? Введите номер или название: ");
+        String choice = scanner.nextLine();
+        if (listBasket.contains(choice)) {
+            System.out.println("Покупка " + choice + " была удалена");
+            listBasket.remove(choice);
+            showBasket();
+        } else {//может быть был введен номер?
+            try {
+                int num = Integer.parseInt(choice);
+                System.out.println("Покупка " + listBasket.get(num - 1) + " была удалена");
+                listBasket.remove(num - 1);
+                showBasket();
+            } catch (IndexOutOfBoundsException | NumberFormatException e) {
+                System.out.println("Указанный продукт " + choice + " не найден!");
+            }
+        }
+    }
     public static void main(String[] args) {
+        listOperation = new ArrayList<>();
+        listOperation.add("Добавить товар");
+        listOperation.add("Показать корзину");
+        listOperation.add("Удалить товар из корзины");
+        listProduct = new ArrayList<>();
+        listProduct.add("Молоко");
+        listProduct.add("Хлеб");
+        listProduct.add("Сыр");
         System.out.println("Добро пожаловать в наш магазин \"Азбука просрочки\"!");
         System.out.println("Для управления своей корзиной вы можете выбрать одну из операций:");
-        System.out.println("1. Добавить товар");
-        System.out.println("2. Показать корзину");
-        System.out.println("3. Удалить товар из корзины");
-        System.out.println("end - выйти из программы");
-        Scanner scanner = new Scanner(System.in);
+        for (String s : listOperation) {
+            System.out.println(listOperation.indexOf(s) + 1 +". " + s);
+        }
+        System.out.println("end - для выхода из программы");
         while (true) {
-            System.out.println("Выберите операцию:");
+            System.out.print("Выберите операцию или введите end: ");
             String choice = scanner.nextLine();
+            switch (choice) {
+                case ("1"):
+                    while (true) {
+                        addProductBasket();
+                        choice = scanner.nextLine();
+                        if (choice.equals("stop")) break;
+                        try {
+                            if (listBasket.contains(listProduct.get(Integer.parseInt(choice) - 1))) {
+                                System.out.println("Такой товар уже выбран!");
+                            } else {
+                                listBasket.add(listProduct.get(Integer.parseInt(choice) - 1));
+                            }
+                        } catch (IndexOutOfBoundsException | NumberFormatException e) {
+                            System.out.println("Такого товара нет в списке!");
+                        }
+                    }
+                    break;
+                case ("2"):
+                    showBasket();
+                    break;
+                case ("3"):
+                    showBasket();
+                    deleteProduct();
+                    break;
+                default:
+                    break;
+            }
             if (choice.equals("end")) break;
         }
         System.out.println("Программа завершена!");
